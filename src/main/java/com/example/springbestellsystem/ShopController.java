1a62cf8e8b5d35948b513fe4b4a9957ae7d45d4b
Zeshan Shahid <z.shahid@hotmail.de>
2023-06-13 15:23:35 +0200

    add ShopController to access Shopsystem via HTTP

--- src/main/java/com/example/springbestellsystem/ShopController.java
+++ src/main/java/com/example/springbestellsystem/ShopController.java
@@ -1,11 +1,39 @@
 package com.example.springbestellsystem;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/shop")
 public class ShopController {
 
+    private final ShopService shopService;
 
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable String id) {
+        return shopService.getProduct(id);
+    }
+
+    @GetMapping
+    public List<Product> products() {
+        return shopService.listAllProducts();
+    }
+
+    @GetMapping("{id}")
+    public Order getOrderById(@PathVariable String id) {
+        return shopService.getOrder(id);
+    }
+
+    @PostMapping
+    public Order addOrder(List<String> productIds) {
+        return shopService.addOrder(productIds);
+    }
+
+    @GetMapping
+    public List<Order> listAllOrders() {
+        return shopService.listAllOrders();
+    }
 }
